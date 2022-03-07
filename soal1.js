2355bfe6726bcc013d165ae46b2d9ac7a312de8d
rikakus <59488349+rikakus@users.noreply.github.com>
2022-03-07 12:45:17 +0700

    Create soal1.js

---
+++ soal1.js
@@ -0,0 +1,38 @@
+
+const biodata = {
+    name: "ian pangestu",
+    age: 19,
+    hobbies:["main game","nonton film","kemah","memancing"],
+    IsMaried: false,
+    schoolList: [
+        {
+            school:"SDN Guworejo 1",
+            yearIn:2009,
+            yearOut:2015,
+            major: null
+        },
+        {
+            school:"SMP N 2 Sragen",
+            yearIn:2015,
+            yearOut:2018,
+            major: null
+        },
+        {
+            school:"SMA N 2 Sragen",
+            yearIn:2018,
+            yearOut:2021,
+            major: "Mipa"
+        },
+    ],
+    Skills:[
+        {
+            skillName:"mengetik",
+            level:"beginner"
+        },
+        {
+            skillName:"mengoding",
+            level:"beginner"
+        }
+    ],
+    interestInCoding: true
+}
