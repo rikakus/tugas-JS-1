74de636d451e3118aa50735083cf0fef6d89f881
rikakus <59488349+rikakus@users.noreply.github.com>
2022-03-09 08:39:44 +0700

    Update Soal2.js

--- Soal2.js
+++ Soal2.js
@@ -1,27 +1,31 @@
-const mtk = 89
-const bahasaIndonesia = 90
-const bahasaInggris = 89
-const ipa =  69
+const mtk = 80;
+const bahasaIndonesia = 80;
+const bahasaInggris = 80;
+const ipa = 80;
 
-if(typeof mtk == "number" && typeof bahasaIndonesia == "number" && typeof bahasaInggris == "number" && typeof ipa == "number"){
-    const jumlah = mtk + bahasaIndonesia + bahasaInggris + ipa
-    const rata2 = jumlah / 4
-    console.log(rata2)
-    if(rata2 <= 100 || rata2 >= 90){
-        console.log("Grade = A")
-    }
-    else if(rata2 <= 89 || rata2 >= 80){
-        console.log("Grade = B")
-    }
-    else if(rata2 <= 79 || rata2 >= 70){
-        console.log("Grade = C")
-    }
-    else if(rata2 <= 69 || rata2 >= 60){
-        console.log("Grade = D")
-    }
-    else{
-        console.log("Grade = E")
-    }
-}else{
-    console.log("anda memasukan selain angka")
+if (
+  typeof mtk == "number" &&
+  typeof bahasaIndonesia == "number" &&
+  typeof bahasaInggris == "number" &&
+  typeof ipa == "number"
+) {
+  const jumlah = mtk + bahasaIndonesia + bahasaInggris + ipa;
+  const rata2 = jumlah / 4;
+
+  if (rata2 > 100) {
+    console.log("tolong masukan nilai yang sesuai");
+  } else if (rata2 <= 100 && rata2 >= 90) {
+    console.log("Grade = A");
+  } else if (rata2 <= 89 && rata2 >= 80) {
+    console.log("Grade = B");
+  } else if (rata2 <= 79 && rata2 >= 70) {
+    console.log("Grade = C");
+  } else if (rata2 <= 69 && rata2 >= 60) {
+    console.log("Grade = D");
+  } else {
+    console.log("Grade = E");
+  }
+  console.log(`Nilai rata-rata = ${rata2}`);
+} else {
+  console.log("anda memasukan selain angka");
 }
